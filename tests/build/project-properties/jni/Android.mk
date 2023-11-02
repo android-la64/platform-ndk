c6961a1943e876a281155460c262e1e53c94a2b5
Wendong <wangwd@xcvmbyte.com>
2023-11-02 06:44:14 +0000

    project-properties for loongarch64 only valid with api>=29.

--- tests/build/project-properties/jni/Android.mk
+++ tests/build/project-properties/jni/Android.mk
@@ -1,7 +1,13 @@
 WANTED_PLATFORM_32 := android-17
 WANTED_PLATFORM_64 := android-21
+WANTED_PLATFORM_loongarch64 := android-29
 
-ifeq (,$(filter %64, $(TARGET_ARCH)))
+ifeq "$(TARGET_ARCH)" "loongarch64"
+  ifneq ($(TARGET_PLATFORM),$(WANTED_PLATFORM_loongarch64))
+    $(error Incorrect target platform: $(TARGET_PLATFORM) (expecteding $(WANTED_PLATFORM_loongarcloongarch64)))
+  endif
+  $(call ndk_log,Test OK: Correct target platform retrieved from project.properties: $(TARGET_PLATFORM))
+else ifeq (,$(filter %64, $(TARGET_ARCH)))
   ifneq ($(TARGET_PLATFORM),$(WANTED_PLATFORM_32))
     $(error Incorrect target platform: $(TARGET_PLATFORM) (expecteding $(WANTED_PLATFORM_32)))
   endif
